6c2a4f3eaedb98cf8d5e124c8564a69ccbb9b66a
TAKAHIRO <takahiro1109tm568@gmail.com>
2021-03-29 05:49:35 +0000

    fix-message

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
 
   belongs_to :user
 
-  validates :title, presence: true, length: {maximum: 20, minimum: 5}
-  validates :body, presence: true, length: {maximum: 50, minimum: 10}
+  validates :title, presence: true, length: {maximum: 30, minimum: 5}
+  validates :body, presence: true, length: {maximum: 150, minimum: 5}
 end
